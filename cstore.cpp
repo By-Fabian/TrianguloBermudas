d0d9b82864e61d56e9ba7c0c813b047fc36043ca
Jose2011 <jose.vidal@utec.edu.pe>
2019-11-01 23:31:04 -0500

    parte de almacen

---
+++ cstore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 1/11/2019.
+//
+
+#include "cstore.h"
---
+++ cstore.h
@@ -0,0 +1,14 @@
+//
+// Created by Usuario on 1/11/2019.
+//
+
+#ifndef TRIANGULOBERMUDAS_CSTORE_H
+#define TRIANGULOBERMUDAS_CSTORE_H
+
+
+class cstore {
+
+};
+
+
+#endif //TRIANGULOBERMUDAS_CSTORE_H
