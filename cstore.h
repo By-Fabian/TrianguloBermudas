863119281f4e628022bce3973411dee5c9a6030b
Jose2011 <jose.vidal@utec.edu.pe>
2019-11-06 10:24:22 -0500

    .

--- cstore.h
+++ cstore.h
@@ -30,12 +30,13 @@ private:
 public:
     //constructor
     cstore();
-    cstore(t_tamano p_tamano_slot; t_producto p_tipo_producto; t_num_product p_cantidad_producto; t_ubicacion p_ubicacion;);
+    cstore(t_tamano p_tamano_slot, t_producto p_tipo_producto, t_num_product p_cantidad_producto, t_ubicacion p_ubicacion);
+    //QUE HACEE
     //metodos setter-recibir
     void set_producto(t_producto producto_in){p_tipo_producto = producto_in;}
     void set_cantidad(t_num_product cantidad_in){p_cantidad_producto = cantidad_in;}
     //metodos getter-dar
-    t_ubicacion get_ubicacion(){ return set_encontrar_producto();}
+    t_ubicacion get_ubicacion(){return set_encontrar_producto();}
 
 };
 
